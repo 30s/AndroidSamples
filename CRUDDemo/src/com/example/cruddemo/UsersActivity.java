f24f4133edf57b8bc97e99de8de0ae78157be2c7
Robert <rzheng@xtremeprog.com>
2012-10-24 10:45:50 +0800

    ListActivity & SimpleCursorAdapter

--- CRUDDemo/src/com/example/cruddemo/UsersActivity.java
+++ CRUDDemo/src/com/example/cruddemo/UsersActivity.java
@@ -1,15 +1,31 @@
 package com.example.cruddemo;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.Contacts.People;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
 
-public class UsersActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
+public class UsersActivity extends ListActivity {
+	
+	private Cursor mCursor;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        mCursor = this.getContentResolver().query(People.CONTENT_URI, null, null, null, null);
+        startManagingCursor(mCursor);
+
+        ListAdapter adapter = new SimpleCursorAdapter(
+                 this,
+                 android.R.layout.two_line_list_item,
+                 mCursor,
+                 new String[] {People.NAME, People.NUMBER},
+                 new int[] {android.R.id.text1, android.R.id.text2});
+
+         setListAdapter(adapter);
     }
 }
