890915612a35ae19439ccad57879e7fd7d26997e
Robert <rzheng@xtremeprog.com>
2012-10-30 14:09:08 +0800

    * Add another test case

---
+++ CRUDDemo/tests/src/com/example/cruddemo/test/DBHelperTest.java
@@ -0,0 +1,10 @@
+package com.example.cruddemo.test;
+
+import android.test.AndroidTestCase;
+
+
+public class DBHelperTest extends AndroidTestCase {
+    public void test_create() {
+        assertTrue(false);
+    }
+}
