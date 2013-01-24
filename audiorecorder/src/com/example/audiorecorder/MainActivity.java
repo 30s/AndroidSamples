0acc4c2a67322548b37346a187707f0d49a7d3ec
Robert <rzheng@xtremeprog.com>
2013-01-24 14:53:36 +0800

    * play audio file

--- audiorecorder/src/com/example/audiorecorder/MainActivity.java
+++ audiorecorder/src/com/example/audiorecorder/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.audiorecorder;
 
+import java.io.File;
 import java.io.IOException;
 
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
@@ -13,6 +16,8 @@ import android.view.View.OnClickListener;
 public class MainActivity extends Activity implements OnClickListener {
 
 	private MediaRecorder mRecorder;
+	private String mSound;
+	private MediaPlayer mPlayer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,13 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_main);
 		findViewById(R.id.btn_start).setOnClickListener(this);
 		findViewById(R.id.btn_stop).setOnClickListener(this);
+		findViewById(R.id.btn_play).setOnClickListener(this);
+		findViewById(R.id.btn_pause).setOnClickListener(this);
+		findViewById(R.id.btn_stop_play).setOnClickListener(this);
+		
 		mRecorder = new MediaRecorder();
+		mSound = Environment.getExternalStorageDirectory()
+				+ "/audiorecorder.3gp";
 	}
 
 	@Override
@@ -37,8 +48,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 			mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
 			mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-			mRecorder.setOutputFile(Environment.getExternalStorageDirectory()
-					+ "/audiorecorder.3gp");
+			mRecorder.setOutputFile(mSound);
 			try {
 				mRecorder.prepare();
 			} catch (IllegalStateException e) {
@@ -51,6 +61,30 @@ public class MainActivity extends Activity implements OnClickListener {
 		case R.id.btn_stop:
 			mRecorder.stop();
 			mRecorder.release();
+			break;
+		case R.id.btn_play:
+			mPlayer = new MediaPlayer();
+			try {
+				mPlayer.setDataSource(mSound);
+				mPlayer.prepare();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			mPlayer.setLooping(true);
+			mPlayer.start();
+			break;
+		case R.id.btn_pause:
+			mPlayer.pause();
+			break;
+		case R.id.btn_stop_play:
+			mPlayer.stop();
+			mPlayer.reset();
 			break;
 		}
 	}
